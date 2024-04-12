f0cb83481fdbcbe3df0a01e453af282945af2ec2
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-04-12 21:30:34 +0400

    "Testing functions, found bug: need to fix" "HTML has too long of variable names, must be more clear and distinct, so that we can filter between numbers and operands"

--- script.js
+++ script.js
@@ -23,9 +23,12 @@ function divide(a = 1, b = 1){
     return(a / b);
 }
 
-let firstNumber;
+let firstNumber = 0;
 let operator;
-let secondNumber;
+let secondNumber = 0;
+
+
+// the operator must somehow store the operator names, i.e: operator should be = addition, or subtraction, etc. 
 
 function operate(firstNumber, operator, secondNumber){
     switch (operator) {
@@ -46,6 +49,29 @@ function operate(firstNumber, operator, secondNumber){
 // old number must be above the new number, so user can see what they are operating on
 //old number smaller, and greyed out, in a separate div
 
+//Defining display variable
+let oldValue = document.querySelector(".oldValue");
+let currentValue = document.querySelector(".currentValue");
+let userInputNumber = document.querySelector(".calculatorButtonsContainer");
+
+//these said variables must change upon user input
+userInputNumber.addEventListener("click", getUserNumber) 
+//need to fix self-induced bug, cut down variable names in HTML to be specific so that getUserNumber can identify which are numbers, operators, and which are functions
+
+
+function getUserNumber(e) {
+    //multiple functions with different purposes: one if a number is clicked, another if an operator is clicked, and another if a function is clicked
+    alert(e.target.className)
+
+    //a user has to be able to click multiple buttons, to allow for multiple digit numbers, and then select the operand, and then the second number
+    };
+
+
+
+// Assign number as per what user has clicked
+function assignNumber(e) {
+
+}
 
 //https://www.w3schools.com/js/js_function_parameters.asp
 //Take time to plan the structure
