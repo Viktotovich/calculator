0ad0c57fb2139309d199d159459e4f059a56f253
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-04-12 14:09:12 +0400

    "General structure, and algorithm"

--- script.js
+++ script.js
@@ -7,21 +7,45 @@
 
 
 //key Functions
-function addition(a, b) {
+function add(a, b) {
     return (a + b);
 }
 
-function subtraction(a, b) {
+function subtract(a, b) {
     return (a - b);
 }
 
-function multiplication(a, b){
+function multiply(a = 1, b = 1){
     return (a * b);
 }
 
-function divide(a, b){
+function divide(a = 1, b = 1){
     return(a / b);
 }
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstNumber, operator, secondNumber){
+    switch (operator) {
+        case addition:
+            add(firstNumber, secondNumber)
+            break;
+        case subtraction:
+            subtract(firstNumber, secondNumber)
+            break;
+        case multiplication:
+            multiply(firstNumber, secondNumber)
+            break;
+        case division: 
+            divide(firstNumber, secondNumber)
+    }
+}
+
+// old number must be above the new number, so user can see what they are operating on
+//old number smaller, and greyed out, in a separate div
+
 
 //https://www.w3schools.com/js/js_function_parameters.asp
 //Take time to plan the structure
