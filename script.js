8d40277b43830d6bd5e0aeea23c9702a910d3669
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-04-13 15:01:08 +0400

    "Added calcs, got rid of 0 problem: caused bugs"
    "Added calculation functionalities and fixed a bug that plagued the whole project with 0's."

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 //key Functions
 function add(a, b) {
-    return parseInt(a + b);
+    total = [+a, +b];
+    return total.reduce((sum, item) => sum += item)
 }
 
 function subtract(a, b) {
@@ -12,8 +13,13 @@ function multiply(a = 1, b = 1){
     return (a * b);
 }
 
-function divide(a = 1, b = 1){
+function divide(a, b){
+    if (a == 0 || b == 0) {
+        alert("it got here")
+        return "Can't Divide 0's"
+    } else {
     return(a / b);
+    }
 }
 
 //key global variables:
@@ -24,41 +30,47 @@ let secondNumber = 0;
 //Selecting display variables
 let oldValue = document.querySelector(".oldValue");
 let currentValue = document.querySelector(".currentValue");
+let calculateButton = document.querySelector(".calculateButton");
+let clear = document.querySelector("#clear");
 
 //Selecting the buttons
 let userInputNumber = document.querySelector(".calculatorButtonsContainer");
 
-//heart of operations, func to callback on every operation
-function operate(firstNumber, operator, secondNumber){
-     switch (operator) {
-        case "+":
-            return add(firstNumber, secondNumber)
-        case "-":
-            return subtract(firstNumber, secondNumber)
-        case "×":
-            return multiply(firstNumber, secondNumber)
-        case "/": 
-            return divide(firstNumber, secondNumber)
-        case "%": 
-            return firstNumber / 100;
-    }
-}
 
 //display morphs if we don't
 currentValue.textContent = 0;
 oldValue.textContent = 0;
 
+clear.addEventListener("click", clearCalculator)
 userInputNumber.addEventListener("click", getUserNumber) 
+calculateButton.addEventListener("click", getResult)
+
+
+//heart of operations, func to callback on every operation
+function operate(firstNumber, operator, secondNumber){
+    switch (operator) {
+       case "+":
+           return add(firstNumber, secondNumber)
+       case "-":
+           return subtract(firstNumber, secondNumber)
+       case "×":
+           return multiply(firstNumber, secondNumber)
+       case "/": 
+           return divide(firstNumber, secondNumber)
+       case "%": 
+           return firstNumber / 100;
+   }
+}
 
 function getUserNumber(e) {
-    //multiple functions with different purposes: one if a number is clicked, another if an operator is clicked, and another if a function is clicked
-
     if (e.target.className == "number") {
+        getRidOfZero();
         if (operator == '') {
             //the a of the equation, is the current number on display
             currentValue.textContent += e.target.textContent;
-            firstNumber = currentValue.textContent;
+            firstNumber = +currentValue.textContent;
         } else {
+            //if operator exists, make the number the b of the equation
             currentValue.textContent += e.target.textContent;
             secondNumber = currentValue.textContent;
         }
@@ -66,13 +78,34 @@ function getUserNumber(e) {
     } else if (e.target.className == "operator") {
             operator = e.target.textContent;
             oldValue.textContent = firstNumber;
-            currentValue.textContent = secondNumber;
+            currentValue.textContent = 0;
         }
     };
 
+// the 0 is causing too many problems, starting from displaying 0x
+function getRidOfZero() {
+    if (currentValue.textContent == 0) {
+        currentValue.textContent = '';
+    }
+}
 
+function getResult() {
+    if (operator != '') {
+        let result = operate(firstNumber, operator, secondNumber)
+        oldValue.textContent = `${firstNumber} ${operator} ${secondNumber} `
+        currentValue.textContent = result;
+        firstNumber = result;
+    }
+}
 
-
+function clearCalculator(){
+    firstNumber = 0;
+    secondNumber = 0;
+    currentValue.textContent = 0;
+    oldValue.textContent = 0;
+    operator = '';
+}
+    
 //a user has to be able to click multiple buttons, to allow for multiple digit numbers, and then select the operand, and then the second number
 
 //the calculation is done only after user clicks the equality button
