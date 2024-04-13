71e9da4b55536f5379b221acdacf81df54d6c15e
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-04-13 12:28:06 +0400

    "Refactored code"

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-
-
-
-//Go for MVP
-//Need time to write algorithm on paper
-
-
 //key Functions
 function add(a, b) {
     return parseInt(a + b);
@@ -23,13 +16,19 @@ function divide(a = 1, b = 1){
     return(a / b);
 }
 
+//key global variables:
 let firstNumber = 0;
-let operator;
+let operator = '';
 let secondNumber = 0;
 
+//Selecting display variables
+let oldValue = document.querySelector(".oldValue");
+let currentValue = document.querySelector(".currentValue");
 
-// the operator must somehow store the operator names, i.e: operator should be = addition, or subtraction, etc. 
+//Selecting the buttons
+let userInputNumber = document.querySelector(".calculatorButtonsContainer");
 
+//heart of operations, func to callback on every operation
 function operate(firstNumber, operator, secondNumber){
      switch (operator) {
         case "+":
@@ -40,45 +39,45 @@ function operate(firstNumber, operator, secondNumber){
             return multiply(firstNumber, secondNumber)
         case "/": 
             return divide(firstNumber, secondNumber)
+        case "%": 
+            return firstNumber / 100;
     }
 }
 
-// old number must be above the new number, so user can see what they are operating on
-//old number smaller, and greyed out, in a separate div
+//display morphs if we don't
+currentValue.textContent = 0;
+oldValue.textContent = 0;
 
-//Defining display variable
-let oldValue = document.querySelector(".oldValue");
-let currentValue = document.querySelector(".currentValue");
-let userInputNumber = document.querySelector(".calculatorButtonsContainer");
-
-//these said variables must change upon user input
 userInputNumber.addEventListener("click", getUserNumber) 
-//need to fix self-induced bug, cut down variable names in HTML to be specific so that getUserNumber can identify which are numbers, operators, and which are functions
-
 
 function getUserNumber(e) {
     //multiple functions with different purposes: one if a number is clicked, another if an operator is clicked, and another if a function is clicked
 
-    //doesnt work as intended, re-write this but take inspiration before deleting
     if (e.target.className == "number") {
-        currentValue.textContent = e.target.textContent;
-        firstNumber = currentValue.textContent;
-    } else if (e.target.className == "operator") {
-        oldValue.textContent = currentValue.textContent;
-        currentValue.textContent = operate(firstNumber, e.target.textContent, secondNumber);
-        secondNumber = currentValue.textContent
+        if (operator == '') {
+            //the a of the equation, is the current number on display
+            currentValue.textContent += e.target.textContent;
+            firstNumber = currentValue.textContent;
+        } else {
+            currentValue.textContent += e.target.textContent;
+            secondNumber = currentValue.textContent;
         }
-    
-
-    //a user has to be able to click multiple buttons, to allow for multiple digit numbers, and then select the operand, and then the second number
+        //operator assign-er is perfect, don't touch
+    } else if (e.target.className == "operator") {
+            operator = e.target.textContent;
+            oldValue.textContent = firstNumber;
+            currentValue.textContent = secondNumber;
+        }
     };
 
 
 
-// Assign number as per what user has clicked
-function assignNumber(e) {
 
-}
+//a user has to be able to click multiple buttons, to allow for multiple digit numbers, and then select the operand, and then the second number
+
+//the calculation is done only after user clicks the equality button
+
+
 
 //https://www.w3schools.com/js/js_function_parameters.asp
 //Take time to plan the structure
